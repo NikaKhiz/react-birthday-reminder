e7c34e855364717f25eb9f27e0af1e4a388a69c9
NikaKhiz <khizambarelinika123@gmail.com>
2022-12-07 15:06:27 +0400

    Add styled components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,72 @@
-import React, { useState } from 'react';
-import {Data} from './Data';
-import List from './List';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Data } from "./Data";
+import List from "./List";
+
+const StyledSection = styled.section`
+  width: 90vw;
+  margin: 0 auto;
+  max-width: var(--max-width);
+
+  @media screen and (min-width: 992px) {
+    width: 95vw;
+  }
+  width: 90vw;
+  margin: 5rem 0;
+  max-width: var(--fixed-width);
+  background: var(--clr-white);
+  border-radius: var(--radius);
+  padding: 1.5rem 2rem;
+  box-shadow: var(--dark-shadow);
+`;
+const StyledMain = styled.main`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const StyledHeading = styled.h3`
+  font-weight: normal;
+  text-transform: none;
+  margin-bottom: 2rem;
+  font-size: 1.25rem;
+  letter-spacing: var(--spacing);
+
+  @media screen and (min-width: 800px) {
+    font-size: 1.75rem;
+    line-height: 1;
+  }
+`;
+const StyledButton = styled.button`
+  color: var(--clr-white);
+  display: block;
+  width: 100%;
+  border-color: transparent;
+  background: var(--clr-pink);
+  margin: 2rem auto 0 auto;
+  text-transform: capitalize;
+  font-size: 1.2rem;
+  padding: 0.5rem 0;
+  letter-spacing: var(--spacing);
+  border-radius: var(--radius);
+  outline: 1px solid rgba(242, 138, 178, 0.8);
+  cursor: pointer;
+`;
+
 function App() {
   const [people, setPeople] = useState(Data);
   return (
-    <main>
-      <section className="container">
-        <h3>{people.length} birthdays today</h3>
-        <List people={people} />
-        <button type="button" className="btn" onClick={() => setPeople([])}>clear all</button>
-      </section>
-    </main>
+    <>
+      <StyledMain>
+        <StyledSection>
+          <StyledHeading>{people.length} birthdays today</StyledHeading>
+          <List people={people} />
+          <StyledButton type="button" onClick={() => setPeople([])}>
+            clear all
+          </StyledButton>
+        </StyledSection>
+      </StyledMain>
+    </>
   );
 }
 
--- src/List.js
+++ src/List.js
@@ -1,4 +1,39 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+
+const StyledArticle = styled.article`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  column-gap: 0.75rem;
+  margin-bottom: 1.5rem;
+  align-items: center;
+`;
+const StyledImg = styled.img`
+  width: 75px;
+  height: 75px;
+  object-fit: cover;
+  border-radius: 50%;
+  box-shadow: var(--light-shadow);
+`;
+const StyledParagraph = styled.p`
+  margin-bottom: 0;
+  color: var(--clr-grey-5);
+  font-size: 0.875rem;
+  @media screen and (min-width: 800px) {
+    font-size: 1rem;
+    line-height: 1.5;
+  }
+`;
+const StyledHeading = styled.h4`
+  margin-bottom: 0.35rem;
+  font-size: 0.875rem;
+  line-height: 1.25;
+  @media screen and (min-width: 800px) {
+    font-size: 1rem;
+    line-height: 1;
+  }
+`;
+const StyledDiv = styled.div``;
 
 const List = ({ people }) => {
   return (
@@ -6,14 +41,13 @@ const List = ({ people }) => {
       {people.map((person) => {
         const { id, name, age, image } = person;
         return (
-          <article className='person' key={id}>
-            <img src={image} alt={name} />
-            <div>
-              <h4>{name}</h4>
-              <p>{age} years</p>
-            </div>
-
-          </article>
+          <StyledArticle key={id}>
+            <StyledImg src={image} alt={name} />
+            <StyledDiv>
+              <StyledHeading>{name}</StyledHeading>
+              <StyledParagraph>{age} years</StyledParagraph>
+            </StyledDiv>
+          </StyledArticle>
         );
       })}
     </>
